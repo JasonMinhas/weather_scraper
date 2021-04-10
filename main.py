254201dc2ab813b5c068930843415472a04d4d23
Jason Minhas <jasonsminhas@gmail.com>
2021-04-09 22:53:06 -0400

    test commit

--- main.py
+++ main.py
@@ -4,9 +4,8 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    pass
 
 
 # Press the green button in the gutter to run the script.
